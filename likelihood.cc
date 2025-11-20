465ebeadfdab9095ecfff67003c840279981883c
Mahtab Mirzaei <mhtbmrz@MacBookAir.fritz.box>
2025-11-20 16:28:42 +0100

    Sheet4_e 16:26

--- likelihood.cc
+++ likelihood.cc
@@ -47,33 +47,28 @@ int main() {
     ofstream fout1("likelihood.txt");
     ofstream fout2("nll.txt");
     ofstream fout3("deltanll.txt");
-    ofstream fout4("rel_deviation.txt");
 
     for (double j: mu)
     {
-        double Likelihood_mu = 1.0;
+        double Likelihood_mu = 1;
+        double saturatedModel = 1;
         for (int k: daten)
         {
             Likelihood_mu *= poisson(j, k);
+            saturatedModel *= poisson(k, k);
         }
         //cout << j << " " << Likelihood_mu << endl;
+
+        double nll = -2*log(Likelihood_mu);
+        double delta_nll = 2*log(Likelihood_mean) - 2*log(Likelihood_mu);
+        double z = (-2*log(Likelihood_mean/saturatedModel) - 233)/sqrt(2*233);
+
         fout1 << j << " " << Likelihood_mu << endl;
-        fout2 << j << " " << -2*log(Likelihood_mu) << endl;
-        fout3 << j << " " << -2*log(Likelihood_mu) + 2*log(Likelihood_mean)<< endl;
+        fout2 << j << " " << nll << endl;
+        fout3 << j << " " << delta_nll << endl;
+        cout << "relative deviation of likelihood ratio from the mean " << z << endl;
         
-        for (double l: daten){
-        double saturatedModel = 1;
-        for (double k: daten)
-        {
-            saturatedModel *= poisson(l, k);
-        }
-        //cout << "for k = " << l << " -> the  likelihood ratio Λ = " << -2*log(Likelihood_mean/saturatedModel) << endl;
-        //cout << l << " " << saturatedModel << endl;
-        double z = (-2*log(Likelihood_mean/saturatedModel) -233)/sqrt(2*233);
-        cout << "z = " << z << endl;
-        //fout4 << z << endl;
-        }
-    }   
+    }  
     fout1.close();
     fout2.close();
     fout3.close();
